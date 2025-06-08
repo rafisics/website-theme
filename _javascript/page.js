e45091762b84b9d6f4130699a0dd7170d1aca0cf
Mubtasim Fuad <m.fuad.rafi@gmail.com>
2025-06-08 22:18:27 +0600

    Update theme

--- _javascript/page.js
+++ _javascript/page.js
@@ -2,19 +2,19 @@ import { basic, initTopbar, initSidebar } from './modules/layouts';
 
 import {
   loadImg,
+  initToc,
   imgPopup,
   initLocaleDatetime,
   initClipboard,
-  initToc,
   loadMermaid
 } from './modules/components';
 
 loadImg();
 initToc();
 imgPopup();
+initTopbar();
 initSidebar();
 // initLocaleDatetime();  // dark-light mode-toggle is getting disabled for it (?)
 initClipboard();
-initTopbar();
 loadMermaid();
 basic();
